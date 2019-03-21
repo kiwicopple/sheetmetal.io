0222e0633fc8313b95d4a015e4a4105727b0a140
Copple <copple@pauls-mbp.lan>
2019-03-21 22:30:39 +0800

    Fixing prettier config

--- components/docs/Snippets.js
+++ components/docs/Snippets.js
@@ -2,57 +2,54 @@ import * as Curl from './snippets/Curl'
 
 const wrapCode = codeString => (
   <div className="code-snippet">
-    <pre >
+    <pre>
       <button className="button is-dark">Copy</button>
-      <code>
-        {codeString}
-      </code>
+      <code>{codeString}</code>
     </pre>
   </div>
 )
 
-export const getSheet = ({language, sheetKey}) => {
+export const getSheet = ({ language, sheetKey }) => {
   switch (language) {
     case 'CURL':
       return wrapCode(Curl.getSheet(sheetKey))
     default:
-      return (<div></div>)
+      return <div />
   }
 }
 
-export const createRecord = ({language, sheetKey, tab}) => {
+export const createRecord = ({ language, sheetKey, tab }) => {
   switch (language) {
     case 'CURL':
       return wrapCode(Curl.createRecord(sheetKey, tab))
     default:
-      return (<div></div>)
+      return <div />
   }
 }
 
-export const retrieveRecords = ({language, sheetKey, tab, range}) => {
+export const retrieveRecords = ({ language, sheetKey, tab, range }) => {
   switch (language) {
     case 'CURL':
       return wrapCode(Curl.retrieveRecords(sheetKey, tab, range))
     default:
-      return (<div></div>)
+      return <div />
   }
 }
 
-export const updateRecord = ({language, sheetKey, tab, range}) => {
+export const updateRecord = ({ language, sheetKey, tab, range }) => {
   switch (language) {
     case 'CURL':
       return wrapCode(Curl.updateRecord(sheetKey, tab, range))
     default:
-      return (<div></div>)
+      return <div />
   }
 }
 
-export const deleteRecords = ({language, sheetKey, tab, range}) => {
+export const deleteRecords = ({ language, sheetKey, tab, range }) => {
   switch (language) {
     case 'CURL':
       return wrapCode(Curl.deleteRecords(sheetKey, tab, range))
     default:
-      return (<div></div>)
+      return <div />
   }
 }
-
