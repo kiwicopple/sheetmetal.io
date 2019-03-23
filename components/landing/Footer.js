5f8c89868bfbefeaca0ae5697282d2c983af199c
Copple <copple@pauls-mbp.lan>
2019-03-23 20:41:15 +0800

    Tweaking Footer

--- components/landing/Footer.js
+++ components/landing/Footer.js
@@ -8,14 +8,20 @@ export default class NavBar extends React.PureComponent {
             <div className="column">
               <img src="/static/img/sheet-metal-logo.png" alt="Sheet Metal" style={{ width: '80%' }} />
             </div>
-            <div className="column has-text">
+            <div className="column">
               <h5 className="heading">About</h5>
               <p>
-                <a href="#">Pricing</a>
+                <a href="/#pricing">Pricing</a>
+              </p>
+              <p>
+                <a href="/docs/">Documentation</a>
               </p>
               <p>
                 <a href="https://github.com/kiwicopple/sheetmetal.io">Github</a>
               </p>
+            </div>
+            <div className="column">
+              <h5 className="heading">Info</h5>
               <p>
                 <a href="/static/humans.txt">Humans</a>
               </p>
